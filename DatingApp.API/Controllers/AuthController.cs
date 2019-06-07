15f4d1a27cb3b20124c5758f078f07f435bee3d8
mleitao <mleitao@cenero.com>
2019-06-06 23:29:30 -0400

    Added Token

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,12 @@ using DatingApp.API.Data;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using DatingApp.API.Dtos;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,18 +17,17 @@ namespace DatingApp.API.Controllers
   public class AuthController : ControllerBase
   {
     private readonly IAuthRepository _repo;
+    private readonly IConfiguration _config;
 
-
-    public AuthController(IAuthRepository repo)
+    public AuthController(IAuthRepository repo, IConfiguration config)
     {
       this._repo = repo;
+      this._config = config;
     }
 
-    [HttpPost]
-    public async Task<ActionResult> Register(UserForRegister userForRegister)
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserForRegister userForRegister)
     {
-        //validate request
-
         userForRegister.Username = userForRegister.Username.ToLower();
 
         if(await _repo.UserExists(userForRegister.Username))
@@ -38,6 +43,36 @@ namespace DatingApp.API.Controllers
         return StatusCode(201);
     }
 
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserForLogin userForLogin)
+    {
+      var userFromRepo = await _repo.Login(userForLogin.Username.ToLower(), userForLogin.Password);
+      if(userFromRepo == null)
+        return Unauthorized();
+
+      var claims = new []{
+        new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+        new Claim(ClaimTypes.Name, userFromRepo.Username),
+      };
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+      var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+      var tokenDescriptor = new SecurityTokenDescriptor{
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = creds
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+
+      return Ok(new {
+        token = tokenHandler.WriteToken(token)
+      });
+    }
 
   }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -44,7 +44,7 @@ namespace DatingApp.API
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
-                app.UseHttpsRedirection();
+                //app.UseHttpsRedirection();
             }
 
             //app.UseHttpsRedirection();
