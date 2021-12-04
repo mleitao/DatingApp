b3d6e774754ef44f226069c9da9df06222025414
Michael Leitao <mleitao@nanthealth.com>
2021-12-03 21:34:19 -0500

    added registration

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AccountService } from 'src/_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() userFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+  
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe(respose => {
+      console.log(respose);
+      this.cancel();
+    }, error => {
+      console.log(error);
+    });
+    
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
